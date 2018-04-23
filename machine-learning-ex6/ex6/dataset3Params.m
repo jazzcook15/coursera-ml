0ab78c33b4f76cfea2587ec30e44aa7862adeadb
Steve Berardi <steve.berardi@synapse.com>
2018-04-22 21:28:37 -0700

    * solve 1.2.3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+try_vec = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0];
+%try_vec = [0.01, 0.1, 1.0, 10.0];
+C_vec = try_vec;
+sig_vec = try_vec;
 
+err_mat = zeros(length(C_vec), length(sig_vec));
 
+for i=1:length(C_vec)
+  C = C_vec(i);
+  for j=1:length(sig_vec)
+    sigma = sig_vec(j);
+    % train a model with these test C and sigma values
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    % evaulate the model's performance
+    pred = svmPredict(model, Xval);
+    err_mat(i, j) = mean(double(pred ~= yval));
+  end
+end
 
-
-
+% determine row in each column has the minimum error for that column
+[min_r,idx_r] = min(err_mat);
+% determine which column of the minimum rows has the minimum error
+[~,idx_c] = min(min_r);
+% select C from the row that minimized the error
+C = C_vec(idx_r(idx_c));
+% select sigma as the column that minimized the error
+sigma = sig_vec(idx_c);
 
 % =========================================================================
 
